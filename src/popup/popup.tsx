006170ed16008f6b56e5d3375fab45eba8b7d7aa
KemalIlkilic <kemalonur.ilkilic@bahcesehir.edu.tr>
2024-01-25 19:50:36 +0300

    Add useEffect hook to fetch weather data and  add OpenWeatherData interface

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import './popup.css'
+import { fetchOpenWeatherData } from '../utils/api'
 
 const App: React.FC<{}> = () => {
+  useEffect(function () {
+    async function getCityData() {
+      try {
+        const data = await fetchOpenWeatherData('London')
+        console.log(data)
+      } catch (error) {
+        console.error(error.message)
+      }
+    }
+    getCityData()
+  }, [])
   return (
     <div>
       <img src="icon.png" />
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,57 @@
 const OPEN_WEATHER_API_KEY = '10f48255c4dfea5833413547c36c97cf'
 
-export async function fetchOpenWeatherData(city: string): Promise<any> {
+export interface OpenWeatherData {
+  coord: {
+    lon: number
+    lat: number
+  }
+  weather: {
+    id: number
+    main: string
+    description: string
+    icon: string
+  }[]
+  base: string
+  main: {
+    temp: number
+    feels_like: number
+    temp_min: number
+    temp_max: number
+    pressure: number
+    humidity: number
+  }
+  visibility: number
+  wind: {
+    speed: number
+    deg: number
+    gust: number
+  }
+  clouds: {
+    all: number
+  }
+  dt: number
+  sys: {
+    type: number
+    id: number
+    country: string
+    sunrise: number
+    sunset: number
+  }
+  timezone: number
+  id: number
+  name: string
+  cod: number
+}
+
+export async function fetchOpenWeatherData(
+  city: string
+): Promise<OpenWeatherData> {
   const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPEN_WEATHER_API_KEY}`
   )
 
   if (!res.ok) throw new Error('City not found')
 
-  const data = await res.json()
+  const data: OpenWeatherData = await res.json()
   return data
 }
