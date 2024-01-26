46951ee638110328892b732dc605a860bb445acc
KemalIlkilic <kemalonur.ilkilic@bahcesehir.edu.tr>
2024-01-26 16:57:15 +0300

    Update popup.css and package.json, add city input field and weather cards

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,16 +1,45 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './popup.css'
 import WeatherCard from './WeatherCard'
 import 'fontsource-roboto'
+import { keys } from '@material-ui/core/styles/createBreakpoints'
+import { Box, InputBase, IconButton, Paper } from '@material-ui/core'
+import AddIcon from '@mui/icons-material/Add'
+import Grid from '@mui/material/Grid' // Grid version 1
 
 const App: React.FC<{}> = () => {
+  const [cities, setCities] = useState<string[]>([
+    'Toronto',
+    'Balikesir',
+    'dasdas',
+  ])
   return (
-    <div>
-      <WeatherCard city="Toronto" />
-      <WeatherCard city="Balikesir" />
-      <WeatherCard city="Bsdasd" />
-    </div>
+    <Box mx="8px" my="16px">
+      <Grid container>
+        <Grid item>
+          <Paper>
+            <Box px="15px" py="5px">
+              <Grid
+                container
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <InputBase placeholder="Add a city name" />
+                <IconButton>
+                  <AddIcon />
+                </IconButton>
+              </Grid>
+            </Box>
+          </Paper>
+        </Grid>
+
+        {cities.map((city, index) => (
+          <WeatherCard city={city} key={index} />
+        ))}
+      </Grid>
+    </Box>
   )
 }
 
